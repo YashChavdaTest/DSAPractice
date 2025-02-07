ffacf5bad81291f740b8cfe26470b4722d577e5e
NileshKasane_i29y78m <nileshkasane15@gmail.com>
2025-02-07 19:26:26 +0530

    Finonacci Series

---
+++ src/nilesh/array/FibonacciSeries.java
@@ -0,0 +1,30 @@
+package nilesh.array;
+
+public class FibonacciSeries {
+
+    public static void main(String[] args) {
+
+        int n = 10;
+        PrintFibonacciSeries(n);
+
+    }
+
+    public static void PrintFibonacciSeries (int n){
+
+        int first = 0;
+        int second = 1;
+
+        System.out.println("Fibonacci Series: " +first+ ", " +second);
+
+        for (int i = 2; i < n; i++){
+
+            int next = first + second;
+            System.out.print(", " +next);
+
+            first = second;
+            second = next;
+        }
+        System.out.println();
+    }
+
+}
